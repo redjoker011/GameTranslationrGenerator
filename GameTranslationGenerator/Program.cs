d5db359802addba7199875f8dd484e25e8009286
pete_jdv <redjoker011@gmail.com>
2022-07-08 23:38:15 +0800

    feat: Create Reader Class

--- GameTranslationGenerator/Program.cs
+++ GameTranslationGenerator/Program.cs
@@ -1,45 +1,43 @@
-﻿using System.IO;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
+﻿namespace GameTranslationGenerator;
+class Program
+{
+	static string FilePath = "";
+	static string Output = "";
 
-namespace GameTranslationGenerator;
-	class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		Console.WriteLine("Initializing Translation Generator....\n");
+
+		var ExitApp = false;
+
+		var output = "/Users/pete_jdv/Downloads/MG/file.yml";
+		var file = "/Users/pete_jdv/Downloads/MG/game-list.csv";
+
+		var parser = new Parser();
+		new Reader(parser).Translate(file, output);
+
+		if (!ExitApp)
 		{
-			Console.WriteLine("Translation Generator");
-
-			var parser = new Parser();
-			using (var reader = new StreamReader(@"/Users/pete_jdv/Downloads/MG/game-list.csv"))
-			{
-				while (!reader.EndOfStream)
-				{
-					var line = reader.ReadLine();
-					var values = line.Split(",");
-
-					// 0 -> Original Name
-					// 1 -> Chinese Translation
-					// 2 -> Game Code
-					// 3 -> Game Type
-
-				    // Parse for English
-					parser.Parse(values[0], values[2], values[3]);
-				    // Parse for Chinese
-					parser.Parse(values[1], values[2], values[3], "cn");
-
-
-					using (var writer = new StreamWriter(@"/Users/pete_jdv/Downloads/MG/file.yml"))
-					{
-						// Save Changes
-						var serializer = new SerializerBuilder()
-						.WithNamingConvention(CamelCaseNamingConvention.Instance)
-						.Build();
-						serializer.Serialize(writer, parser);
-					}
-				}
-			}
-
 			Console.WriteLine("File Generation Done!, Press Any Key to Exit");
 			Console.ReadLine();
 		}
 	}
+	static void CheckFilePath()
+	{
+		if(String.IsNullOrEmpty(FilePath))
+		{
+			Console.WriteLine("Missing argument `filePath`");
+			return;
+		}
+    }
+
+	static void CheckOutput()
+	{
+		if(String.IsNullOrEmpty(FilePath))
+		{
+			Console.WriteLine("Missing argument `output`");
+			return;
+		}
+    }
+
+}
---
+++ GameTranslationGenerator/Reader.cs
@@ -0,0 +1,53 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace GameTranslationGenerator;
+
+public class Reader
+{
+	Parser Parser { get; set; }
+
+    public Reader(Parser Service)
+    {
+
+		Parser = Service;
+    }
+
+    public void Translate(string FilePath, string Output)
+    {
+		using (var reader = new StreamReader($@"{FilePath}"))
+		{
+			while (!reader.EndOfStream)
+			{
+				var line = reader.ReadLine();
+				var values = line.Split(",");
+
+				// 0 -> Original Name
+				// 1 -> Chinese Translation
+				// 2 -> Game Code
+				// 3 -> Game Type
+
+			    // Parse for English
+				Parser.Parse(values[0], values[2], values[3]);
+			    // Parse for Chinese
+				Parser.Parse(values[1], values[2], values[3], "cn");
+
+
+				WriteToFile(Output);
+			}
+		}
+    }
+
+	private void WriteToFile(string Output)
+	{
+		using (var writer = new StreamWriter($@"{Output}"))
+		{
+			// Save Changes
+			var serializer = new SerializerBuilder()
+			.WithNamingConvention(CamelCaseNamingConvention.Instance)
+			.Build();
+			serializer.Serialize(writer, Parser);
+		}
+    }
+}
+
